d7540f4682b151203b3bc7de004c15437ad7d23d
seniorgato <ebates1794@hotmail.com>
2015-09-16 22:52:38 +0000

    Added Features of Fitness Tracker to readme.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # WebProgFall2015
 Web Programming 2015 Assignments and Projects
+Features for Fitness Tracker Website
+    - Personalized Account set up (i.e. Name, gender, height, weight, etc.)
+    - Calender style homepage for users
+    - caloric intake meter with option to input consumed foods
+    - exercise inputs 
+    - progress tab
+    - workout diy 
+    - meal diy
--- index.php
+++ index.php
@@ -11,100 +11,79 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   </head>
   <body>
-         <div class= "container">
-           <h1>Hello, world!</h1>
-           <div class= "row">
-             
-             <div class= "col-sm-8">
-               
-                                           
-                          
-                          
-                          
-                         <button type="button" class="btn btn-success btn-lg"> <I class="glyphicon glyphicon-plus" ></I>New record
-                         
-                         </button>
-                         
-                         
-                         
-                         <button type="button" class="btn btn-danger btn-lg"> <I class="glyphicon glyphicon-trash" ></I>Delete all
-                         <span class="badge">4</span>
-                         
-                         </button>     
-                          
-                          
-                          <table class="table table-hover">
-                            <thead>
-                              <tr>
-                                <th>#</th>
-                                <th>First Name</th>
-                                <th>Last Name</th>
-                                <th>Username</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                              <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                              </tr>
-                              <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                              </tr>
-                              <tr>
-                                <th scope="row">3</th>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>@twitter</td>
-                              </tr>
-                            </tbody>
-                          </table>
-    
-               
-               
-             </div>
-             <div class= "col-xs-4">
-                            
-                              <div class="alert alert-success "container" " role="alert">You did well</div>
-                              <div class="alert alert-info ""container" role="alert">:D</div>
-                         
-                               
-                 
-                 
-                              <div class="progress ""container">
-                              <div class="progress-bar progress-bar-striped active container" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 15%">
-                      <span class="sr-only">45% Complete</span>
-                      
-                        </div>
-                      </div>    
-            
-               
-             </div>
-           </div>
-           
-     
-    
+    <div class="container">
+            <h1>Hello, world!</h1>
+      <div class="row">
+        <div class="col-md-8 col-xs-10">
+            <a href="#" class="btn btn-success">
+                <i class="glyphicon glyphicon-plus"></i>
+                New Record
+            </a>
+            <a href="#" class="btn btn-danger">
+                <i class="glyphicon glyphicon-trash"></i>
+                Delete All
+                <span class="badge">4</span>
+            </a>
+            <table class="table table-condensed table-striped table-bordered table-hover">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Username</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>Larry</td>
+                  <td>the Bird</td>
+                  <td>@twitter</td>
+                </tr>
+              </tbody>
+            </table>  
+          
         </div>
-        
-        
+        <div class="col-md-4 col-xs-2">
+            <span class="label label-success">Success</span>
+            <span class="label label-info">Info</span>
+            <span class="label label-warning">Warning</span>
+        </div>
+        <div class="col-md-4 col-xs-10">
+            <div class="alert alert-success" role="alert">
+                You did well
+            </div>
+            <div class="alert alert-danger" role="alert">
+                Oh no! You messed up.
+            </div>
+            <div class="progress">
+              <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 15%">
+                <span >45% Complete</span>
+              </div>
+            </div>
+          
+        </div>
+      </div>
+      
             
-       
-            
-     
-        
-        
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    </div>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>\
-    <script type="text/javascript" src="">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
       $(".progress-bar").animate({width: "75%"}, 2000);
-        
     </script>
-    
   </body>
 </html>
