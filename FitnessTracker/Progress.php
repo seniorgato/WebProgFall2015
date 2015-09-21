bf2fe9d963bfa8b6c6846499ab535fb06e57fa75
seniorgato <ebates1794@hotmail.com>
2015-09-21 05:39:27 +0000

    Fitness Tracker Folder with 3 Web Pages for Website (php+css included)

---
+++ FitnessTracker/Progress.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+          <link rel="stylesheet" href="Progress.css">
+
+    <img class= "iconlogo1" src="https://www.veganmainstream.com/wp-content/uploads/2011/11/healthandfitness.jpg">
+  </head>
+  <body>
+      
+    <div class= "container">
+                        <div>
+                             <nav class="navbar navbar-default">
+                                      <div class="container-fluid">
+                                        <!-- Brand and toggle get grouped for better mobile display -->
+                                        <div class="navbar-header">
+                                          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                            <span class="sr-only">Toggle navigation</span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                          </button>
+                                          <a class="navbar-brand" rel="home" href="#" title="tddesigns">
+                                          
+                                            Home
+                                        </a>
+                                        </div>
+                                    
+                                        <!-- Collect the nav links, forms, and other content for toggling -->
+                                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                                          <ul class="nav navbar-nav">
+                                            <li><a href="#">Food</a></li>
+                                            <li><a href="#">Exercise</a></li>
+                                            <li><a href="#">Progress</a></li>
+                                            <li><a href="#">Community</a></li>
+                                            <li><a href="#">Log In</a></li>
+                                            
+                                          </ul>
+                                          <ul class="nav navbar-nav navbar-right">
+                                            <li><a href="#">Donations</a></li>
+                                            <li class="dropdown">
+                                              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Related Websites <span class="caret"></span></a>
+                                              <ul class="dropdown-menu">
+                                                <li><a href="#">Fitness.com</a></li>
+                                                <li><a href="#">Nutrition.com</a></li>
+                                                <li><a href="#">com.com</a></li>
+                                                <li role="separator" class="divider"></li>
+                                                
+                                              </ul>
+                                            </li>
+                                          </ul>
+                                        </div><!-- /.navbar-collapse -->
+                                      </div><!-- /.container-fluid -->
+                             </nav> 
+                         </div>
+                         
+                         
+                                         <div class="input-group">
+                                              <span class="input-group-btn">
+                                                <button class="btn btn-default" type="button">Go!</button>
+                                              </span>
+                                              <input type="text" class="form-control" placeholder="Enter Dish">
+                                            </div><!-- /input-group -->
+                                         
+                         
+                         
+                                            <div class="jumbotron">
+                                                
+                                                <center style="color: white; padding-top:75px;">
+                                                    <h1>Workout Today<small style="color: white">For a Better Tomorrow</small></h1>
+                                                </center>
+                                                
+                                            </div>
+                                            
+                                            <div id="Progress of Account">
+                                                <h1>Almost done with Account Info</h1>
+                                                <div class="progress">
+                                                      <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                                                        60%
+                                                      </div>
+                                                    </div>
+                                                
+                                            </div>
+                                            
+                                            
+                                            <div class="progress">
+                                          <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                                            <span class="sr-only">40% Complete (success)</span>
+                                            Calorie
+                                          </div>
+                                        </div>
+                                        <div class="progress">
+                                          <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
+                                            <span class="sr-only">20% Complete</span>
+                                            Exercise for the Day
+                                          </div>
+                                        </div>
+                                        <div class="progress">
+                                          <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+                                            <span class="sr-only">60% Complete (warning)</span>
+                                            Weight
+                                          </div>
+                                        </div>
+                                        <div class="progress">
+                                          <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+                                            <span class="sr-only">80% Complete (danger)</span>
+                                            Consumption
+                                          </div>
+                                        </div>
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+                                            
+    </div>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+    </script>
+  </body>
+</html>
