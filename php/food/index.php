60dfeb24b47d8062cf1c69c1d58cbbfc5cf85f0f
seniorgato <ebates1794@hotmail.com>
2015-09-24 14:45:49 +0000

    Learning php

--- php/food/index.php
+++ php/food/index.php
@@ -1,3 +1,23 @@
+<?php 
+
+    $name= 'Erik Bates'; /*'' are string literals*/
+    $message= "Welcome $name"; /*"" evaluates whats in the string*/
+    
+    $person = array('Name' => $name, 'Age' => 21.5, 'CalorieGoal' => 2000 );
+    $food = array(
+        
+        array('Name'=>'Breakfast', 'Time'=> strtotime("one hour ago"), Calories=> 400),
+        array('Name'=>'Lunch', 'Time'=> strtotime("now"), Calories=> 800),
+        array('Name'=>'Snack', 'Time'=> strtotime("now"), Calories=> 250),
+        array('Name'=>'Dinner', 'Time'=> strtotime("6pm"), Calories=> 800),
+        
+        );
+    $total =0;
+    foreach($food as $meal){  /*flipped around. the meal is the iterative variable*/
+        $total += $meal['Calories'];
+    }
+    
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -5,14 +25,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>Food Intake</title>
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   </head>
   <body>
     <div class="container">
-            <h1>Hello, world!</h1>
+            <h1>Food Intake</h1>
+            <h2><?=$message?></h2>
+            <div class="panel panel-success">Successful Butts</div>
+            <div class= "panel-heading">Your Data</div>
+            <div class = "panel-body">
+                <dt>
+                    Name
+                </dt>
+                <dd><?=$person['Name']?></dd>
+                <dt>
+                    Age
+                </dt>
+                <dd><?=$person['Age']?></dd>
+                <dt>
+                    Today's Intake
+                </dt>
+                <dd><?=$total?></dd>
+            </div>
+            
+            
+            
       <div class="row">
         <div class="col-md-8 col-xs-10">
             <a href="#" class="btn btn-success">
@@ -27,18 +67,22 @@
             <table class="table table-condensed table-striped table-bordered table-hover">
               <thead>
                 <tr>
-                  <th>#</th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Username</th>
+                 <th>#</th>
+                  <th>Time</th>
+                  <th>Name</th>
+                  <th>Calories</th>
+                  
                 </tr>
               </thead>
+              
               <tbody>
+                  <?php foreach($food as $meal):?>
                 <tr>
                   <th scope="row">1</th>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
+                  <td><?=$meal ['Name']?></td>
+                  <td><?=$meal ['Time']?></td>
+                  <td><?=$meal ['Calories']?></td>
+
                 </tr>
                 <tr>
                   <th scope="row">2</th>
@@ -52,14 +96,10 @@
                   <td>the Bird</td>
                   <td>@twitter</td>
                 </tr>
+                <?php endforeach;?>
               </tbody>
             </table>  
           
-        </div>
-        <div class="col-md-4 col-xs-2">
-            <span class="label label-success">Success</span>
-            <span class="label label-info">Info</span>
-            <span class="label label-warning">Warning</span>
         </div>
         <div class="col-md-4 col-xs-10">
             <div class="alert alert-success" role="alert">
@@ -68,11 +108,7 @@
             <div class="alert alert-danger" role="alert">
                 Oh no! You messed up.
             </div>
-            <div class="progress">
-              <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 15%">
-                <span >45% Complete</span>
-              </div>
-            </div>
+           
           
         </div>
       </div>
